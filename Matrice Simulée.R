062f9e9b3b2df79b3bb35706f7bc26584bdfc507
gradufle <85991485+gradufle@users.noreply.github.com>
2024-08-06 16:46:17 +0200

    Update Matrice Simulée.R

--- Matrice Simulée.R
+++ Matrice Simulée.R
@@ -1,7 +1,6 @@
 main <- read_dta("main.dta")
 
 af <- data.frame(main,c("ghm1","ghm2","finessgeo1","finessgeo2","annee_sortie1"))
-
 af <- main %>% select(ghm1,ghm2,finessgeo1,finessgeo2,annee_sortie1)
 View(af)
 
@@ -12,15 +11,12 @@ View(af)
 set.seed(1)
 
 original_matrix <- matrix(round(rexp(36, rate = 0.7)*10), nrow = 6, ncol = 6)
-
 new_diag <- apply(original_matrix, 2, sum) * round(rexp(1, rate = 0.9)*3)
-
 diag(original_matrix) <- new_diag
 print(original_matrix)
 
 n <- 5
 m <- 5
-
 # Function to create a new matrix with the value distributed
 distribute_value <- function(value, n, m) {
   new_matrix <- matrix(0, nrow = n, ncol = m)
@@ -40,26 +36,20 @@ distribute_value <- function(value, n, m) {
 }
 # List to store all new matrices
 all_new_matrices <- list()
-
 # Loop through each element in the original matrix
 for (i in 1:nrow(original_matrix)) {
   for (j in 1:ncol(original_matrix)) {
     value <- original_matrix[i, j]
     new_matrix <- distribute_value(value, n, m)
     new_matrix <- reshape2::melt(new_matrix)
-    
     IJ <- matrix(c(i,j) , byrow = TRUE, nrow = n*m , ncol=2) 
     new_matrix2 <- cbind(new_matrix, IJ)
-    
     all_new_matrices[[paste0("Matrix_", i, "_", j)]] <- new_matrix2
     Lmat <- cbind(all_new_matrices)
   }
 }
-
 # Display the list of new matrices
 all_new_matrices
 print(new_matrix2)
-
-alltodo <- matrix(all_new_matrices)
-
-print(alltodo)
+MDS <- do.call(rbind.data.frame, all_new_matrices)
+print(MDS)
